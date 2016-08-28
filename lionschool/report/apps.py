c95e6bc51dda5c2cb2cfcaead0be0319477391cd
Leander Lismond <Leo2807@users.noreply.github.com>
2016-08-28 13:20:26 +0200

    Fix module namespacing
    Because the lionschool module is split across multiple directories
    we have to use module namespacing, but that also requires us to
    specify the path variable in our app config. Expect more problems
    to come!

--- lionschool/report/apps.py
+++ lionschool/report/apps.py
@@ -1,5 +1,10 @@
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
+import os
 
 
 class LionreportConfig(AppConfig):
-    name = 'lionreport'
+    name = 'lionschool.report'
+    label = 'lionreport'
+    verbose_name = _("Reports")
+    path = os.path.dirname(os.path.abspath(__file__))
