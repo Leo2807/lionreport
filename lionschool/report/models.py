787aaf4a53ec95d8f46f21cca1d9b6416c3a587c
Leander Lismond <Leo2807@users.noreply.github.com>
2016-08-31 10:06:31 +0200

    Add Evaluation and Result models
    Include tests, admin, migrations and translations

--- lionschool/report/models.py
+++ lionschool/report/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+from lionschool.core.models import Pupil, Teacher, Warden, Course
+
+
+class Evaluation(models.Model):
+    """This class represents an evaluation held. Not to be confused with the
+    individual results of said evaluation, represented as :model: `Result`"""
+    course = models.ForeignKey(Course, verbose_name=_("course"), blank=True)
+    title = models.CharField(_("title"), max_length=150)
+    desc = models.TextField(_("description"), blank=True)
+    date = models.DateField(_("date"))
+
+    def __str__(self):
+        return str(self.title)
+
+    class Meta:
+        verbose_name = _("evaluation")
+        verbose_name_plural = _("evaluations")
+
+
+class Result(models.Model):
+    """This class represents the individual results of a student, the overall
+    evaluation is stored in Evaluation"""
+    evaluation = models.ForeignKey(
+        Evaluation, verbose_name=_("evaluation"), on_delete=models.CASCADE
+    )
+    score = models.PositiveIntegerField(
+        verbose_name=_("score"), blank=True, null=True,
+        validators=[MaxValueValidator(100)]
+    )
+    comment = models.TextField(_("comment"), blank=True)
+    pupil = models.ForeignKey(Pupil, verbose_name=_("pupil"))
+
+    class Meta:
+        verbose_name = _('result')
+        verbose_name_plural = _('results')
+
+    def failed(self):
+        return self.score < 50
+
+    def __str__(self):
+        return str(self.evaluation)
--- lionschool/report/tests.py
+++ lionschool/report/tests.py
@@ -1,3 +1,42 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Evaluation, Result
+
+
+class EvaluationTest(TestCase):
+    def test_correct_string_representation(self):
+        title = "dfghjkdsf&&é"
+        evaluation = Evaluation(title=title)
+        self.assertEqual(title, str(evaluation))
+
+
+class ResultTest(TestCase):
+    def test_correct_string_representation(self):
+        title = "dfghjkdsf&&é"
+        evaluation = Evaluation(title=title)
+        result = Result(evaluation=evaluation)
+        self.assertEqual(title, str(result))
+
+    def test_succeed_when_score_over_50(self):
+        score = 75
+        result = Result(score=score)
+        self.assertIs(
+            False, result.failed(),
+            "Result isn't succeeded when score is {}".format(score)
+        )
+
+    def test_fail_when_score_under_50(self):
+        score = 25
+        result = Result(score=score)
+        self.assertIs(
+            True, result.failed(),
+            "Result isn't failed when score is {}".format(score)
+        )
+
+    def test_succeed_when_score_fifty(self):
+        score = 50
+        result = Result(score=score)
+        self.assertIs(
+            False, result.failed(),
+            "Result isn't succeeded when score is {}".format(score)
+        )
